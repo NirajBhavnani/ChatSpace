9cec2f58c3f16dad623b70edc4032e00f1011509
nbhavna <niraj.bhavnani@morningstar.com>
2022-08-01 22:02:02 +0530

    Created login and signup pages

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <form @submit.prevent="signIn">
+    <input type="email" required placeholder="Email" v-model="email" />
+    <input type="password" required placeholder="Password" v-model="password" />
+    <button>Log In</button>
+  </form>
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+export default {
+  setup() {
+    // refs
+    const email = ref("");
+    const password = ref("");
+
+    const signIn = () => {
+      console.log(email, password);
+    };
+
+    return { email, password, signIn };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <form @submit.prevent="signUp">
+    <input
+      type="text"
+      required
+      placeholder="Display name"
+      v-model="displayName"
+    />
+    <input type="email" required placeholder="Email" v-model="email" />
+    <input type="password" required placeholder="Password" v-model="password" />
+    <button>Sign Up</button>
+  </form>
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+export default {
+  setup() {
+    // refs
+    const displayName = ref("");
+    const email = ref("");
+    const password = ref("");
+
+    const signUp = () => {
+      console.log(displayName, email, password);
+    };
+
+    return { displayName, email, password, signUp };
+  },
+};
+</script>
+
+<style>
+</style>
