b10f97e8bfe2551f68b0fe811d23088d7124036d
nbhavna <niraj.bhavnani@morningstar.com>
2022-08-01 21:28:50 +0530

    Project setup

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZxe7AMC2ki6Zj7ZHTuEKIB7GEvIHdKlk",
+  authDomain: "vue-firebase-sites-36b5a.firebaseapp.com",
+  projectId: "vue-firebase-sites-36b5a",
+  storageBucket: "vue-firebase-sites-36b5a.appspot.com",
+  messagingSenderId: "133477645271",
+  appId: "1:133477645271:web:fe22165a5599e9f367e992",
+};
+
+//   firebase init
+firebase.initializeApp(firebaseConfig);
+
+// firestore init
+const firestore = firebase.firestore();
+
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { firestore, timestamp };
